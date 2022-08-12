3057b01e577f53562db621871803a12941f30ec2
Heriberto Hernandez Torres <heriberto@gercanada123>
2022-08-12 17:17:21 -0500

    first commit

---
+++ vue-apirender/src/store/index.js
@@ -0,0 +1,78 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+import axios from "axios";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost";
+
+// Create a new store instance.
+const store = createStore({
+    state() {
+        return {
+            user: null,
+        }
+    },
+    mutations: {
+        SET_USER(state, value) {
+            state.user = value
+        }
+    },
+    actions: {
+        async login({ dispatch }, credentials) {
+            await axios.get("/sanctum/csrf-cookie");
+            await axios.post("/login", credentials);
+            return dispatch("getUser");
+        },
+
+        getUser({ commit }) {
+            axios.get("api/user")
+                .then((res) => {
+                    commit('SET_USER', res.data)
+                        // this.user = { 'email': 1, 'password': 2 };
+                        // this.user = res.data;
+                }).catch(() => {
+                    commit('SET_USER', null)
+                });
+        }
+    },
+});
+
+const app = createApp({ /* your root component */ })
+
+// Install the store instance as a plugin
+
+export default store;
+// app.use(store)
+
+/*             Vue.use(Vuex);
+
+            import axios from "axios";
+            axios.defaults.withCredentials = true;
+            axios.defaults.baseURL = "http://localhost";
+            export default new Vuex.Store({
+                state: { user: null, },
+                mutetions: {
+                    SET_USER(state, value) {
+                        state.user = value
+                    }
+                },
+                actions: {
+                    async login({ dispatch }, credentials) {
+                        await axios.get("/sanctum/csrf-cookie");
+                        await axios.post("/login", credentials);
+                        return dispatch("getUser");
+                    },
+
+                    getUser({ commit }) {
+                        axios.get("api/user")
+                            .then((res) => {
+                                commit('SET_USER', res.data)
+                                    // this.user = { 'email': 1, 'password': 2 };
+                                    // this.user = res.data;
+                            }).catch(() => {
+                                commit('SET_USER', null)
+                            });
+                    }
+                },
+                modules: {}
+            });
+            app.use(store); */
---
+++ vue-apirender/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <!-- <button @click="me()">Me</button> -->
+
+    <div v-if="user">
+      <!-- <button @click="logout()">Log out</button> -->
+      <!-- <h1>Usuartio actual : {{ user.email }}</h1> -->
+    </div>
+    <!-- <pre>{{ user }}</pre> -->
+    <form action="" @submit.prevent="login" v-else>
+      <input type="email" v-model="form.email" placeholder="email" />
+      <input type="password" v-model="form.password" placeholder="password" />
+      <br />
+      <button>Login</button>
+    </form>
+  </div>
+  <div></div>
+</template>
+<script>
+export default {
+  data: () => ({
+    // user: null,
+    form: {
+      email: "jl@gmail.com",
+      password: "password",
+    },
+  }),
+
+  methods: {
+    /*     logout() {
+      axios.post("/logout").then(() => {
+        this.user = null;
+        console.log("exit");
+      });
+      console.log("Enviando login", this.form);
+    }, */
+    async login() {
+      await this.$store.dispatch("login", this.form);
+    },
+    /*  me() {
+      
+    }, */
+  },
+};
+</script>
